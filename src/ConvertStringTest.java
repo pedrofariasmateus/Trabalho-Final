e2fddb95469db28ae7283b7af28387e184a5da5c
JoaoAiveca <joaiveca@hotmail.com>
2010-11-20 19:39:13 +0000

    Método que permite separar as componentes de uma String, e o teste que verifica a validade do mesmo.
    Signed-off-by: JoaoAiveca <joaiveca@hotmail.com>

---
+++ src/ConvertStringTest.java
@@ -0,0 +1,21 @@
+import junit.framework.TestCase;
+import java.util.ArrayList;
+
+public class ConvertStringTest extends TestCase{
+	
+	String toConvert="Yes, I hope you die a miserable painful death";
+	ConvertString underTest=new ConvertString();
+	
+	public void test1(){
+		ArrayList<String> x=underTest.Converter(toConvert);
+		String temp="";
+		int t=x.size();
+		for(int i=0; i<t-1; i++){
+			temp=temp+x.get(i) + " ";}
+		temp=temp+x.get(t-1);
+		
+		assertEquals(toConvert, temp);
+	}
+
+	
+}
