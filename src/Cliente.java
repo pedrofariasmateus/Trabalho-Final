3756f744ea14eb531174cacf43e74c7fdfeae6ad
Pedro Mateus <pedrofariasmateus@hotmail.com>
2010-11-23 17:48:59 +0000

    PUSH!!!!!!
    Signed-off-by: Pedro Mateus <pedrofariasmateus@hotmail.com>

--- src/Cliente.java
+++ src/Cliente.java
@@ -30,5 +30,51 @@ public class Cliente {
 		
 		return toPrint;
 	}
-	
+	public void setName(String n){
+		this.name=n;
+		nameA=(ConvertString.Converter(name));
+		
+	}
+	public void setAddress(String a){
+		this.address=a;
+		addressA=(ConvertString.Converter(address));
+	}
+	public void setMainPhone(String p){
+		this.mainPhone=p;
+	}
+	public void modEmail(String ein, String eout){
+		for(int a=0; a<email.size();a++){
+			if(this.email.get(a).equals(eout)==true){
+				this.email.set(a,eout);
+			}
+		}
+	}
+	public void modPhones(String pin, String pout){
+		for(int a=0; a<phones.size();a++){
+			if(this.phones.get(a).equals(pout)==true){
+				this.phones.set(a,pout);
+			}
+		}
+	}
+	public String getNome(){
+		return this.name;
+	}
+	public String getAddress(){
+		return this.address;
+	}
+	public String getMainPhone(){
+		return this.mainPhone;
+	}
+	public ArrayList<String> getPhones(){
+		return this.phones;
+	}
+	public ArrayList<String> getEmail(){
+		return this.email;
+	}
+	public void addPhones(String p){
+		this.phones.add(p);
+	}
+	public void addEmail(String e){
+		this.email.add(e);
+	}
 }
