7fd20f56a3105a00b4fb9b757227e3eacd275812
Pedro Mateus <pedrofariasmateus@hotmail.com>
2010-11-23 18:01:33 +0000

    PUSH!!!UNTIL U POOP!!!!!!!
    Signed-off-by: Pedro Mateus <pedrofariasmateus@hotmail.com>

--- src/Database.java
+++ src/Database.java
@@ -21,6 +21,48 @@ public class Database {
 		return toRet;
 	}
 
+	ArrayList<Cliente> search(String input , String tipo) {
+	
+		ArrayList<String> inp=new ArrayList<String>();
+		if(tipo.equals("address")==true||tipo.equals("name")==true)
+			inp=ConvertString.Converter(input);
+		
+		
+		int count=0;
+		ArrayList<Cliente> toReturn=new ArrayList<Cliente>();
+		for(int i=0; i<data.size(); i++){
+			Cliente temp=data.get(i);
+			
+			ArrayList<String> inpTemp = null;
+			if(tipo.equals("name")==true){
+				inpTemp=temp.nameA;}
+			if(tipo.equals("address")==true){
+				inpTemp=temp.addressA;}
+			
+			if(tipo.equals("address")==true||tipo.equals("name")==true){
+			for(int j=0; j<inp.size(); j++){
+				for(int k=0; k<inpTemp.size(); k++){
+					String a=inpTemp.get(k);
+					String b=inp.get(j);
+					boolean x=a.equals(b);
+					if(x==true){
+						count++;
+					}
+				}
+			}
+			if(count==inp.size())
+				toReturn.add(temp);
+			count=0;
+			}
+			else
+			{
+				
+				
+			}
+				
+		}
+		return toReturn;
+	}
 	
 	void modify(Cliente oldStr, Cliente newStr) {
 
@@ -29,6 +71,5 @@ public class Database {
 	void modify(String dados, int position, Cliente c) {
 	}
 	
-
-	
 }
+
