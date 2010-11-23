a98a694aedd3b584bd01ae1921db591b9ad8859b
marlene <marleneoliveira7@gmail.com>
2010-11-23 12:16:23 +0000

    Signed-off-by: marlene <marleneoliveira7@gmail.com>

---
+++ src/Gestor.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Gestor {
+	
+	public static void main(String args []){
+		
+		Menu m = new Menu();
+		Sistema sis = new Sistema();
+		Database d = new Database();
+		
+		m.mainMenu();
+		//Cliente c = d.getCliente();
+		//c.toString();
+		
+		
+		
+		
+		
+		
+			/*Database a=new Database();
+			Cliente c1=new Cliente("João Aiveca", "Bairro N", null, null, null);
+			Cliente c2=new Cliente("Pedro ", "Evora N",null, null, null);
+			Cliente c3=new Cliente("Marlene ", "Bairro N", null, null, null);
+			Cliente c4=new Cliente("Xlhéu ", "chocolate N", null, null, null);
+			
+			a.add(c1);
+			a.add(c2);
+			a.add(c3);
+			//System.out.println(a.getDB());
+			a.modify(c2, c4);
+			//System.out.println(a.getDB());
+			Cliente c5=a.searchByName("João Aiveca");
+			//System.out.println(c5);
+			a.modify(c4, c5);
+			a.modify("not João", 0, c1);
+			
+			
+			//a.modify(c2, c1);
+			//System.out.println(a.getDB());
+			//System.out.println(c5);*/
+		}
+	
+	
+}
--- src/Sistema.java
+++ src/Sistema.java
@@ -1,27 +1,84 @@
+import java.util.*;
 
 public class Sistema {
 	
-public static void main(String[]args){
-	Database a=new Database();
-	Cliente c1=new Cliente("João Aiveca", "Bairro N", null, null, null);
-	Cliente c2=new Cliente("Pedro ", "Evora N",null, null, null);
-	Cliente c3=new Cliente("Marlene ", "Bairro N", null, null, null);
-	Cliente c4=new Cliente("Xlhéu ", "chocolate N", null, null, null);
+	Scanner s = new Scanner(System.in);
+	String atribute;
+	Database d = new Database();
 	
-	a.add(c1);
-	a.add(c2);
-	a.add(c3);
-	//System.out.println(a.getDB());
-	a.modify(c2, c4);
-	//System.out.println(a.getDB());
-	Cliente c5=a.searchByName("João Aiveca");
-	//System.out.println(c5);
-	a.modify(c4, c5);
-	a.modify("not João", 0, c1);
+	public Cliente insert(){
+		String n,m;
+		String p;
+		String e;
+		String ap;
+		Cliente client;
+		
+		System.out.println("Por favor insira um nome:");
+		n=s.next();
+		System.out.println("Por favor insira uma morada:");
+		m=s.next();
+		System.out.println("Por favor insira um número de telefone:");
+		p=s.next();
 	
-	
-	//a.modify(c2, c1);
-	//System.out.println(a.getDB());
-	//System.out.println(c5);
+		System.out.println("Por favor insira um e-mail:");
+		e=s.next();
+		System.out.println("Por favor insira um telefone alternativo:");
+		ap=s.next();
+		
+		client = new Cliente(n,m,p,e,ap);
+		d.add(client);
+		
+		return client;
+		
 	}
+
+
+	
+	public void modify(Cliente c){
+		String n,m,e;
+		String p,ap;
+		
+		System.out.println("Qual o atributo que deseja alterar?");
+			atribute=s.next();
+			
+		if((atribute.equals("Nome"))||atribute.equals("nome")){
+			System.out.println("Insira o novo nome:");
+			n=s.next();
+			c.add(0,n);
+			
+		}
+		if((atribute.equals("Morada"))||atribute.equals("morada")){
+			System.out.println("Insira a nova morada");
+			m=s.next();
+			c.add(1,m);
+			
+		}
+		if((atribute.equals("Telefone"))||(atribute.equals("telefone"))){
+			System.out.println("Insira um novo número de telefone:");
+			p=s.next();
+			c.add(2,p);
+			
+		}
+		if((atribute.equals("Telefone Alternativo"))||(atribute.equals("telefone alternativo"))){
+			System.out.println("Insira um novo número de telefone alternativo:");
+			ap=s.next();
+			c.add(3,ap);
+			
+		}
+		if((atribute.equals("Email"))||(atribute.equals("email"))||(atribute.equals("E-mail"))||(atribute.equals("e-mail"))){
+			System.out.println("Insira um novo e-mail:");
+			e=s.next();
+			c.add(4,e);
+			
+		}
+	}
+	
+	public void search(){
+		
+		System.out.println("Pesquisar: ");
+	
+		String search = s.next();
+		d.searchByName(search);
+	}
+
 }
